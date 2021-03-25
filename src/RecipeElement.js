00b3e2a739b330494328008f8daa7f62738e2132
SamheinDM <SamheinDM@yandex.ru>
2021-03-25 13:21:16 +0300

    Work on right pannel

--- src/RecipeElement.js
+++ src/RecipeElement.js
@@ -3,7 +3,9 @@ import React from 'react';
 export default class RecipeElement extends React.Component {
   render () {
     return (
-      <div className="recipies_list_elem">
+      <div 
+        className="recipies_list_elem" 
+        onClick={(e)this.props.onClick}>
         {this.props.name}
       </div>
     );
