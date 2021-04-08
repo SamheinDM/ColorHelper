379fce859edcd6f0932527534194c30b361c544f
SamheinDM <SamheinDM@yandex.ru>
2021-04-08 21:28:18 +0300

    Preparing for autoupdate

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 const electron = require('electron');
 const app = electron.app;
 
+const squirrelUrl = "http://localhost:3333";
 if (handleSquirrelEvent(app)) {
     // squirrel event handled and app will exit in 1000ms, so don't do anything else
     return;
@@ -44,6 +45,11 @@ function createWindow() {
         mainWindow = null
     })
 }
+
+app.on('ready', function (){
+    // Add this condition to avoid error when running your application locally
+    if (process.env.NODE_ENV !== "dev") startAutoUpdater(squirrelUrl)
+  });
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -98,6 +104,24 @@ ipcMain.on('update-recipe', (_event, data) => {
 ipcMain.on('delete-recipe', (_event, recipe) => {
     dbAPI.deleteRecipe(recipe);
 });
+
+const startAutoUpdater = (squirrelUrl) => {
+    // The Squirrel application will watch the provided URL
+    electron.autoUpdater.setFeedURL(`${squirrelUrl}/win64/`);
+  
+    // Display a success message on successful update
+    electron.autoUpdater.addListener("update-downloaded", (event, releaseNotes, releaseName) => {
+      electron.dialog.showMessageBox({"message": `The release ${releaseName} has been downloaded`});
+    });
+  
+    // Display an error message on update error
+    electron.autoUpdater.addListener("error", (error) => {
+      electron.dialog.showMessageBox({"message": "Auto updater error: " + error});
+    });
+  
+    // tell squirrel to check for updates
+    electron.autoUpdater.checkForUpdates();
+  }
 
 function handleSquirrelEvent(application) {
     if (process.argv.length === 1) {
